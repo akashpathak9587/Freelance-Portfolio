c3daa4948f4ed1fa149dfc57b97df164c967cc26
Akash Pathak <aakapa95@gmail.com>
2024-12-28 14:09:30 +0530

    screenContextType update

--- src/context/WindowContext/WindowContext.tsx
+++ src/context/WindowContext/WindowContext.tsx
@@ -1,37 +1,46 @@
-import { ReactNode, useEffect, useMemo, useState } from "react";
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { createContext } from "react";
 
 type ScreenContextType = {
-  width: number,
-  isMobile: boolean,
-  section: string,
-  setSection: any,
-}
+  width: number;
+  isMobile: boolean;
+  section: string;
+  setSection: Dispatch<SetStateAction<string>> | null;
+};
 
 export const ScreenContext = createContext<ScreenContextType>({
   width: window.innerWidth,
   isMobile: false,
   section: "",
-  setSection: undefined
+  setSection: null,
 });
 
 const WindowContext = ({ children }: { children: ReactNode }) => {
   const [width, setWidth] = useState<number>(window.innerWidth);
   const [isMobile, setIsMobile] = useState<boolean>(false);
   const [section, setSection] = useState<string>("");
-  
 
   useEffect(() => {
     setIsMobile(width <= 600);
-  }, [width])
+  }, [width]);
 
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  }, [])
+  }, []);
 
-  const contextValue = useMemo(() => ({ width, isMobile, section, setSection }), [width, isMobile, section]);
+  const contextValue = useMemo(
+    () => ({ width, isMobile, section, setSection }),
+    [width, isMobile, section]
+  );
   return (
     <ScreenContext.Provider value={contextValue}>
       {children}
